4e986d0e9b4874717c7302ca947c6597a84f76b4
niagaratriskelion <niagaratriskelion1968@gmail.com>
2024-03-09 11:00:04 -0500

    test

--- tao/assets/js/tao.js
+++ tao/assets/js/tao.js
@@ -76,7 +76,8 @@ function createRow(member) {
     membersTpl.appendChild(rowEl)
 
 }
-function onSignIn(googleUser) {
+function handleCredentialResponse(googleUser) {
+    debugger
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
@@ -84,7 +85,7 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 function signOut() {
-    debugger
+
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
