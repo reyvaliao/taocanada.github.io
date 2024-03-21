f6a1154a384b45ecece076d321503eecf67f619a
rikriki <frederick.rosales@gmail.com>
2024-03-20 20:34:09 -0400

    update register

--- assets/js/register.js
+++ assets/js/register.js
@@ -41,9 +41,25 @@ function checkForms() {
 
     })
     if (valid) {
-        const formData = {}
-        requiredFields.filter(v !== 'col' || v !== 'photo').forEach(v => {
-            formData[v] = document.querySelector('#' + v).value
+
+        const data = new FormData();
+        requiredFields.filter(v => v !== 'col' || v !== 'photo').forEach(v => {
+            data.append(v, document.querySelector('#' + v).value);
+        })
+        data.append('photo', document.getElementById("col").files[0])
+        data.append('col', document.getElementById("col").files[0])
+
+
+        register(data).then((data) => {
+            console.log(data)
+            if (data.code === 200) {
+                document.cookie = "token=" + data.msg;
+                window.location.href = './members-restricted.html'
+
+            } else {
+                document.querySelector('.error-message').textContent = "Username and Password is incorrect"
+                document.querySelector('.error-message').style.display = "block"
+            }
         })
 
     }
@@ -61,3 +77,22 @@ function checkForms() {
         const fileList = this.files; /* now you can work with the file list */
     }
 })();
+
+async function register(data) {
+
+    const response = await fetch('http://localhost:8080/profile-upload-multiple', {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
+        // headers: {
+        //     "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        // redirect: "follow", // manual, *follow, error
+        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: data // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+
+}
