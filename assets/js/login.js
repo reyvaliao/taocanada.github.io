748690af916565069a766e777e73aeccfecbec9e
rikriki <frederick.rosales@gmail.com>
2024-03-22 14:53:50 -0400

    json

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,6 +5,9 @@ window.membersEmail = new Map();
 async function getEmails() {
     const response = await fetch('http://ec2-54-173-229-172.compute-1.amazonaws.com:8080/api/members/emails', {
         method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            "Content-Type": "application/json"
+        },
         redirect: "follow", // manual, *follow, error
         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     });
